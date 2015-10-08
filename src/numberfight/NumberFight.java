cfadd271db905c1f5390ead1c246e9fbfc705fc8
vennila <baby.nila05@gmail.com>
2015-10-08 21:51:35 +0100

    "Initial Commit"

---
+++ src/numberfight/NumberFight.java
@@ -0,0 +1,25 @@
+
+package numberfight;
+
+import java.util.Scanner;
+
+
+public class NumberFight {
+
+    
+    public static void main(String[] args) {
+        int M,B;
+        Scanner numberInput = new Scanner(System.in);
+        System.out.print("Michel, Enter your number? ");
+        M = numberInput.nextInt();
+        System.out.print("Bruce, Enter your number? ");
+        B = numberInput.nextInt();
+        
+        if(M>B){
+            System.out.print("Micheal you won the girl");
+        } else {
+            System.out.print("Bruce you won the girl");
+        }
+    }
+    
+}
